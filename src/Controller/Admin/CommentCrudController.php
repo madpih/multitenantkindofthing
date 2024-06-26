a8eb08c48e02ac958c963af5dd11d590d03fc206
Madis Pihlasalu <madis.pihlasalu@we.ee>
2024-06-26 16:23:43 +0300

    add admin backend, user interface

---
+++ src/Controller/Admin/CommentCrudController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Comment;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\EntityFilter;
+
+class CommentCrudController extends AbstractCrudController
+{
+  public static function getEntityFqcn(): string
+  {
+    return Comment::class;
+  }
+
+  public function configureCrud(Crud $crud): Crud
+  {
+    return $crud
+      ->setEntityLabelInSingular('Conference Comment')
+      ->setEntityLabelInPlural('Conference Comments')
+      ->setSearchFields(['author', 'text', 'email'])
+      ->setDefaultSort(['createdAt' => 'DESC'])
+      ;
+  }
+
+  public function configureFilters(Filters $filters): Filters
+  {
+    return $filters
+      ->add(EntityFilter::new('conference'));
+  }
+
+  public function configureFields(string $pageName): iterable
+  {
+    $fields = parent::configureFields($pageName);
+    $fields[] = AssociationField::new('conference');
+    return $fields;
+  }
+
+}
---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Conference;
+use App\Entity\Comment;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+
+class DashboardController extends AbstractDashboardController
+{
+    #[Route('/admin', name: 'admin')]
+    public function index(): Response
+    {
+        $routeBuilder = $this->container->get(AdminUrlGenerator::class);
+        $url = $routeBuilder->setController(ConferenceCrudController::class)->generateUrl();
+
+        return $this->redirect($url);
+
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Guestbook');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToRoute('Back to the website', 'fa fa-home','homepage');
+        yield MenuItem::linkToCrud('Conferences', 'fas fa-map-market-alt', Conference::class);
+        yield MenuItem::linkToCrud('Comments', 'fas fa-comments',Comment::class);
+    }
+}
